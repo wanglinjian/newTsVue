ea024e48c6716ac2163ce638007248b0eea196d0
wanglinjian <1258975911@qq.com>
2023-04-25 10:03:17 +0800

    Update submit.js

--- submit.js
+++ submit.js
@@ -1,1 +1,14 @@
 模拟数据
+const formItems = data.data.case.formData.formItems || [];
+
+/* 提交数据 */
+const params = {
+  caseId: '',
+  originalData: {}, 
+}
+
+formItems.forEach(item => {
+  if (item.formItemId === 'image_upload-1679307507021') {
+    params.originalData[item.formItemId] = '图片数组数据';
+  } else params.originalData[item.formItemId] = item.value;
+})
